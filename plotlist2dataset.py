c8432fd44f1f801800bc00bf5e44cbe205d3be06
Vide Karlsson <vide.karlsson@gmail.com>
2015-12-22 13:00:28 +0100

    added_deimeterwith_doc_id

--- plotlist2dataset.py
+++ plotlist2dataset.py
@@ -5,12 +5,13 @@ import parse_imdb_annotations as annotation
 ROOT_FOLDER = 'imdb_plot_data_set'
 TEST_DATA_FOLDER = "test_data"
 TRAIN_DATA_FOLDER = "train_data"
-#PLOT_LIST_FILE_NAME = 'test_plot_list.txt' 
-#PLOT_LIST_FILE_NAME = 'plot_list_short.txt' 
+# PLOT_LIST_FILE_NAME = 'test_plot_list.txt' 
+# PLOT_LIST_FILE_NAME = 'plot_list_short.txt' 
 PLOT_LIST_FILE_NAME = 'plot.list' 
 TRAIN_DATA_FILE_NO_DELIMETER = 'imdb_plot_data_set/all_plots_train_no_delimeter'
 TRAIN_DATA_FILE_DELIMETER = 'imdb_plot_data_set/all_plots_train_delimeter'
 NO_CATEGORY_FOLDER = "no_category"
+DELIMETER = "-------------"
 
 def is_title_line(line):
     title_line_matcher = '^MV:.*' 
@@ -80,7 +81,7 @@ def mv_line_to_title_string(title_string):
     title_string = title_string.strip()
     return title_string
 
-def create_data_set(plot_list_file_name, root_folder, test_folder, train_folder, no_category_folder, train_data_file_delimeter,train_data_file_no_delimeter):
+def create_data_set(plot_list_file_name, root_folder, test_folder, train_folder, no_category_folder, train_data_file_delimeter,train_data_file_no_delimeter, delimeter):
     test_folder = os.path.join(root_folder, test_folder)
     train_folder = os.path.join(root_folder, train_folder)
     no_category_folder = os.path.join(test_folder, no_category_folder)
@@ -94,6 +95,7 @@ def create_data_set(plot_list_file_name, root_folder, test_folder, train_folder,
     title_string = None
     plot_lines = []
     file_name = None
+    train_doc_id = 0
     #pdb.set_trace()
     with open(plot_list_file_name, encoding="ISO-8859-1") as f:
         for line in f:
@@ -103,14 +105,16 @@ def create_data_set(plot_list_file_name, root_folder, test_folder, train_folder,
             elif is_plot_line(line):
                 plot_lines.append(line)
             elif is_delimeter_line(line) and file_name:
-                letters_in_plot = [item for sublist in plot_lines for item in sublist]
-                if(len(letters_in_plot)>160):
-                    if  file_name in annotated_movies:
-                        categories = annotation_map[file_name]
-                        print(categories)
-                        print_test_data_plot(plot_lines, file_name, annotated_movies, categories, test_folder, train_folder, no_category_folder)
-                    else:
-                        print_train_data_plot(plot_lines,file_name,train_folder,train_data_file_delimeter,train_data_file_no_delimeter,line)                                       
+                if  file_name in annotated_movies:
+                    # Is test data
+                    categories = annotation_map[file_name]
+                    print(categories)
+                    print_test_data_plot(plot_lines, file_name, annotated_movies, categories, test_folder, train_folder, no_category_folder)
+                else:
+                    # Is train data
+                    doc_delimeter = delimeter+str(train_doc_id)
+                    print_train_data_plot(plot_lines, str(train_doc_id),train_folder,train_data_file_delimeter,train_data_file_no_delimeter, doc_delimeter)                                       
+                    train_doc_id +=1
                 # reset variables
                 title_string = None
                 plot_lines = []
@@ -118,4 +122,4 @@ def create_data_set(plot_list_file_name, root_folder, test_folder, train_folder,
             else:
                 pass
 
-create_data_set(PLOT_LIST_FILE_NAME, ROOT_FOLDER, TEST_DATA_FOLDER, TRAIN_DATA_FOLDER, NO_CATEGORY_FOLDER, TRAIN_DATA_FILE_DELIMETER, TRAIN_DATA_FILE_NO_DELIMETER)
+create_data_set(PLOT_LIST_FILE_NAME, ROOT_FOLDER, TEST_DATA_FOLDER, TRAIN_DATA_FOLDER, NO_CATEGORY_FOLDER, TRAIN_DATA_FILE_DELIMETER, TRAIN_DATA_FILE_NO_DELIMETER, DELIMETER)
